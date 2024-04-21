9b55f1f6eca93a0962b41ef944521614e67ce155
hiki7 <rsaliyev03@gmail.com>
2024-04-21 19:51:57 +0500

    delete: unnecessary folder

--- libapp-project/src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-// src/app/models/user.ts
-export interface User {
-    id?: number;  // Optional if assigned by the backend
-    username: string;
-    email: string;
-    password: string;
-  }
-  
