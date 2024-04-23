fd16530d78504b0411d849dae06f7f520b0441dd
Maksat <mukan.maksat22@gmail.com>
2024-04-24 01:22:51 +0600

    fixing

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('books/', BooksListAPIView.as_view()),
     path('books/<int:id>', BookAPIView.as_view()),
     path('users/', UserListAPIView.as_view()),
-    path('categories/', category_list),
+    path('categorieы/', category_list),
     path('categories/<int:id>', category_details),
     path('api/token/', token_obtain_view, name='token_obtain_pair'),
     path('api/token/refresh/', token_refresh_view, name='token_refresh'),
